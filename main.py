33c0a3477333ff6a00ab8a640b262a1b68455246
kakaa2993@ <kakaa2993@gmail.com>
2022-08-02 11:29:02 -0700

    add the movinig function to the program

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
 from turtle import Turtle,Screen
-
+import time
+import random
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Snake Game")
 screen.bgcolor("Black")
 starting_position = [(0, 0), (-20, 0), (-40, 0), ]
+segments = []
+screen.tracer(0)
+random_color = ["red","green","blue"]
+i = 0
 for position in starting_position:
-    turtle = Turtle(shape="square")
-    turtle.color("White")
+    seg = Turtle(shape="square")
+    seg.penup()
+    seg.color("white")
+    # seg.color(random_color[i])
     # turtle.shapesize(stretch_wid=0.5,stretch_len=.5,outline=0)
-    turtle.goto(position)
+    seg.goto(position)
+    segments.append(seg)
+    i += 1
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for segment_num in range(len(segments) - 1, 0, -1):
+        segment_before = segments[segment_num - 1]
+        x_position = segment_before.xcor()
+        y_position = segment_before.ycor()
+        segments[segment_num].goto(x=x_position,y=y_position)
+    segments[0].forward(20)
+
 
 
 screen.exitonclick()
