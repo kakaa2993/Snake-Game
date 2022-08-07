53ef906ede59b228e3f792f414aa16869524115a
kakaa2993@ <kakaa2993@gmail.com>
2022-08-07 00:20:40 -0700

    add high score feature to trak the best score you have ashieved

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-from turtle import Screen
 import time
-from snake import Snake
+from turtle import Screen
 from food import Food
 from scoreboard import Scoreboard
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -34,13 +34,18 @@ while game_is_on:
 
     # Detect the collision with the wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.game_over()
-        game_is_on = False
+        snake.reset()
+        scoreboard.reset()
+        scoreboard.update_scoreboard()
+        # game_is_on = False
+        # scoreboard.game_over()
 
     # Detect if there is a collision between the segments
     for segment in snake.segments[1::]:
         if snake.head.distance(segment) < 10:
-            scoreboard.game_over()
-            game_is_on = False
+            # scoreboard.game_over()
+            # game_is_on = False
+            snake.reset()
+            scoreboard.update_scoreboard()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -8,23 +8,31 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.hideturtle()
         self.color("white")
-        self.scoreboard()
+        self.goto(x=0, y=260)
+        self.update_scoreboard()
         self.speed("fastest")
 
-    def scoreboard(self):
-        self.goto(x=0, y=260)
+    def update_scoreboard(self):
         self.clear()
-        self.write(arg=f"Score: {self.score}", move=False, align= ALIGN, font= FONT)
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", move=False, align= ALIGN, font= FONT)
         self.color('chartreuse')
 
-    def game_over(self):
-        self.color("red")
-        self.goto(x=0, y=0)
-        self.write(arg="Game Over", move=False, align=ALIGN, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+
+
+
+    # def game_over(self):
+    #     self.color("red")
+    #     self.goto(x=0, y=0)
+    #     self.write(arg="Game Over", move=False, align=ALIGN, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.scoreboard()
+        self.update_scoreboard()
